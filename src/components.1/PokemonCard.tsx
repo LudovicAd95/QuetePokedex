0f11c82d0b5a1462d8d89bf8cdb063bd30a3052f
Ludovic Plougonven <ludo.plougonven@gmail.com>
2024-10-23 17:08:50 +0200

    CreateCard2

--- src/components.1/PokemonCard.tsx
+++ src/components.1/PokemonCard.tsx
@@ -1,13 +1,34 @@
-import React from "react";
 
-const PokemonCard = () => {
-
-    return ( 
+const pokemonList = [
+    {
+      name: "Bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "Mew",
+    },
+  ];
+  
+  function PokemonCard() {
+    let pokemon = pokemonList[0];
+  
+    const imgValid = pokemon.imgSrc;
+  
+    return (
+      imgValid ? (
         <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur"/>
-        <figcaption> Bulbasaur </figcaption>
+          <img src={pokemon.imgSrc} alt={pokemon.name}/>
+          <figcaption>{pokemon.name}</figcaption>
         </figure>
+      ) : (
+        <figure>
+          <p>???</p>
+          <figcaption>{pokemon.name}</figcaption>
+        </figure>
+      )
     );
   }
   
   export default PokemonCard;
+  
