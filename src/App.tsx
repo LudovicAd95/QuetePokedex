81d7314f060d437c7d14f02ce53f35c46ff6761f
Ludovic Plougonven <ludo.plougonven@gmail.com>
2024-10-24 11:23:22 +0200

    bouton

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
 import "./App.css";
 import PokemonCard from './components.1/PokemonCard';
-
+import { useState } from "react";
 
 const pokemonList = [
   {
-    name: "Bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "Mew",
-
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 const App = () => {
 
+  const [count, setcount] = useState(0);
+
+  const handleClickPrecedent = () => {
+    if (count > 0) {
+      setcount(count -1)
+    }
+  };
+
+  const handleClickSuivant = () => {
+    if (count < pokemonList.length -1) {
+      setcount(count +1)
+    }
+  };
 
   return (
+    <section>
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemonList[count]} />
+      <p>{count}</p>
+      <button onClick={handleClickPrecedent}>Precedent</button>
+      <button onClick={handleClickSuivant}>Suivant</button>
     </div>
+    </section>
   );
 };
 
